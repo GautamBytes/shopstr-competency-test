497654de88d572ae02fbf947ec5cb2a6d6c30fad
Gautam Manchandani <manchandanigautam@gmail.com>
2025-04-08 16:18:22 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
